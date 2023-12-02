0815ec198c4ad7c58ea3345d4971bf6327e135dd
dhetalisa <dhetalisa@gmail.com>
2023-12-02 18:14:13 +0700

    first commmit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+  <div className='text-center bg-black '>
+    <p className='font-bold'>copyright 2023 codesakti. All right reserved.</p>
+  </div>   
+
+  )
+}
+
+export default Footer 
---
+++ src/app/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {}
+
+const Profile = (props: Props) => {
+  return (
+    <div className='h-[500px] flex flex-col justify-center text-center text-black px-52 space-y-10'>
+        <div className='space-y-4'>
+            <p className='text-3xl font-bold'>Gambaran Umum</p>
+            <p className='font-extralight'>Himpunan Mahasiswa Teknik Informatika (HMIF) - Institut Teknologi Indonesia merupakan organisasi mahasiswa program studi Teknik Informatika di Institut Teknologi Indonesia yang bertujuan mengembangkan potensi mahasiswa Teknik Informatika dengan berlandaskan Tri Dharma Perguruan Tinggi</p>
+        </div>
+        <div>
+            <p className='text-base font-semibold text-gray-500 uppercase'>Tujuan</p>
+            <p className='text-3xl font-bold pb-4'>Melangkah Bersama Menuju Kemajuan</p>
+            <p className='font-extralight'>Menjadikan HMIF sebagai wadah bagi anggota maupun calon anggota untuk mengembangkan diri dalam bidang akademik maupun non akademik dan menjadikan HMIF ITI menjadi organisasi yang aktif solutif dan produkif secara internal maupun eksternal demi terwujudnya Tri Dharma Perguruan Tinggi</p>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
